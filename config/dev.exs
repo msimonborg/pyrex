046754c9db9a1ff9ce6b05045bf54de32d9aa982
Matthew Borg <msimonborg@gmail.com>
2022-07-05 11:00:34 -0400

    add components dir to live_reload file watcher

--- config/dev.exs
+++ config/dev.exs
@@ -61,7 +61,7 @@ config :pyrex, PyrexWeb.Endpoint,
     patterns: [
       ~r"priv/static/.*(js|css|png|jpeg|jpg|gif|svg)$",
       ~r"priv/gettext/.*(po)$",
-      ~r"lib/pyrex_web/(live|views)/.*(ex)$",
+      ~r"lib/pyrex_web/(live|views|components)/.*(ex)$",
       ~r"lib/pyrex_web/templates/.*(eex)$"
     ]
   ]
